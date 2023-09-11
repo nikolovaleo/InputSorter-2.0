684c4d17084aa54e58cced12580de6cf8a8456fc
Jaime <47996736+jaime385@users.noreply.github.com>
2023-09-11 17:55:06 -0600

    Functionality is finished
    The main objective of the code is now finished. We need to work on handling errors and some quality related topics of the final product.

--- main.py
+++ main.py
@@ -66,11 +66,13 @@ def compare_command():
         shortcutList = [item[0] for item in shortcutListRaw]
     print("The following shortcuts are present of the CSV file: ")
     print(shortcutList)
-    match_shortcut = []
+    match_shortcut_csv = []
+    match_shortcut_conditional_csv = False
     for item in shortcutList:
         if item in ShortcutFinal:
-            match_shortcut.append(item)
-            print("There is a match! The " + match_shortcut[0] + " shortcut has been detected on the CSV file")
+            match_shortcut_csv.append(item)
+            match_shortcut_conditional_csv = True
+            print("There is a match! The " + match_shortcut_csv[0] + " shortcut has been detected on the CSV file")
 
 
     # .docx files name list
@@ -85,29 +87,33 @@ def compare_command():
     print(docx_files)
     
 
-    # Replace 'your_document.docx' with the path to your .docx file
-    docx_path = 'c:/Users/E1430967/Documents/GitHub/InputSorter-2.0/Data/initial.docx'
-    
-    # Initialize a COM object for Microsoft Word
-    word = win32com.client.Dispatch("Word.Application")
+    match_shortcut_docx = []
+    match_shortcut_conditional_docx = False
+    for i in docx_files:
+        if i in ShortcutFinal:
+            print(i)
+            match_shortcut_docx.append(i)
+            match_shortcut_conditional_docx = True
+            print("There is a match! The " + match_shortcut_docx[0] + " shortcut has been detected on the data folder")
 
-    # Open the .docx file
-    doc = word.Documents.Open(docx_path)
 
-    # Select the entire document
-    doc.Content.WholeStory
+    if match_shortcut_conditional_csv and match_shortcut_conditional_docx:
+        # Replace 'your_document.docx' with the path to your .docx file
+        docx_path = "c:/Users/E1430967/Documents/GitHub/InputSorter-2.0/Data/" + ShortcutFinal + ".docx"
+        # Initialize a COM object for Microsoft Word
+        word = win32com.client.Dispatch("Word.Application")
+        # Open the .docx file
+        doc = word.Documents.Open(docx_path)
+        # Select the entire document
+        doc.Content.WholeStory
+        # Copy the selected content to the clipboard
+        doc.Content.Copy()
+        # Close the document without saving changes
+        doc.Close(False)
+        # Quit Microsoft Word
+        word.Quit()
+        print("Content copied to clipboard using pywin32.")
 
-    # Copy the selected content to the clipboard
-    doc.Content.Copy()
-
-    # Close the document without saving changes
-    doc.Close(False)
-
-    # Quit Microsoft Word
-    word.Quit()
-
-    print("Content copied to clipboard using pywin32.")
-    
 
 
 if __name__ == "__main__":
@@ -126,3 +132,6 @@ if __name__ == "__main__":
 
 
 #sru shortcut
+
+#sdfds
+#initial
